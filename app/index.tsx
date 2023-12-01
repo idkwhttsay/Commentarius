f4c5f5af457862d17c8793702afefd367a3533f4
idkwhattosay <92638932+idkwhttsay@users.noreply.github.com>
2023-12-01 20:38:15 +0600

    last changes

--- app/index.tsx
+++ app/index.tsx
@@ -1,36 +1,43 @@
-import { View, Text, StyleSheet, Button, Animated, Easing, Image } from 'react-native';
-import React, { useEffect } from 'react';
+import { View, Text, StyleSheet, Button, Platform } from 'react-native';
+import React, { useState, useEffect } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { useCallback } from 'react';
-import { Link } from 'expo-router';
+import { ExpoRoot, router } from 'expo-router';
+import LottieView from 'lottie-react-native';
+import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-google-signin/google-signin';
+import auth, { FirebaseAuthTypes, firebase } from '@react-native-firebase/auth';
+import db from "@react-native-firebase/database";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function HomePage() {
-    const spinValue = new Animated.Value(0);
+    const webClientId = "499505743608-3ml0o4ia4k15kim4invd4jctcih60p9f.apps.googleusercontent.com"; 
 
-    const rotate = spinValue.interpolate({
-      inputRange: [0, 1],
-      outputRange: ['0deg', '360deg'],
-    });
-    
-    const spin = () => {
-      spinValue.setValue(0),
-      Animated.timing(spinValue, {
-        toValue: 1,
-        duration: 4000,
-        easing: Easing.linear,
-        useNativeDriver: true,
-      }).start(() => spin());
-    }
+    useEffect(()=>{
+        GoogleSignin.configure({
+            webClientId: webClientId,
+        })
+    },[])
 
-    useEffect(() => {
-      spin();
-    }, []);
-
-    const icon = require('../assets/images/adaptive-icon.png');
+    const googleLogin = async () => {
+        try {
+            await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+            const { idToken } = await GoogleSignin.signIn();
+            // console.log("idToken:", idToken);
+        } catch (error: any) {
+            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+                console.log(error)
+            } else if (error.code === statusCodes.IN_PROGRESS) {
+                console.log(error)
+            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+                console.log(error)
+            } else {
+                console.log(error.message)
+            }
+        }
+    };
 
     const [fontsLoaded] = useFonts({
       'Jost-Regular': require('../assets/fonts/Jost-Regular.ttf'),
@@ -46,68 +53,75 @@ export default function HomePage() {
       return null;
     }
     
-    return (
-        <SafeAreaView style={styles.view}>
-            <View style={styles.container}>
-              <Animated.View style={{transform: [{rotate}]}}>
-                <Image style={{
-                  flex: 1,
-                  resizeMode: 'center'
-                }} source={icon}/>
-              </Animated.View>
-            </View>
-            <Text style={styles.big}>Welcome!</Text>
-            <View style={{ flexDirection: 'row'}}>
-                <Text style={styles.log}>Log into your </Text>
-                <Text
-                    style={{
-                        color: '#4285F4',
-                        fontSize: 28,
-                        fontFamily: 'Jost-Regular',
-                    }}
-                >G</Text>
-                <Text
-                    style={{
-                        color: '#DB4437',
-                        fontSize: 28,
-                        fontFamily: 'Jost-Regular',
-                    }}
-                >o</Text>
-                <Text
-                    style={{
-                        color: '#F4B400',
-                        fontSize: 28,
-                        fontFamily: 'Jost-Regular',
-                    }}
-                >o</Text>
-                <Text
-                    style={{
-                        color: '#4285F4',
-                        fontSize: 28,
-                        fontFamily: 'Jost-Regular',
-                    }}
-                >g</Text>
-                <Text
-                    style={{
-                        color: '#0F9D58',
-                        fontSize: 28,
-                        fontFamily: 'Jost-Regular',
-                    }}
-                >l</Text>
-                <Text
-                    style={{
-                        color: '#DB4437',
-                        fontSize: 28,
-                        fontFamily: 'Jost-Regular',
-                    }}
-                >e{' '}</Text>
-                <Text style={styles.log}>account</Text>
-            </View>
-            <Link style={{flex: 1}} href="/Current/CurrentWeek" asChild>
-                <Button title="Login with Google acoount" />
-            </Link>
-        </SafeAreaView>
-    );
+        return (
+            <SafeAreaView style={styles.view}>
+                <View style={{
+                    flex: 2,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}>
+                    <LottieView 
+                        source={require('../assets/images/Loader.json')}
+                        autoPlay
+                        loop={true}
+                        style={{width: 400, height: 400}}
+                    />
+                </View>
+                <Text style={styles.big}>Welcome!</Text>
+                <View style={{ flexDirection: 'row'}}>
+                    <Text style={styles.log}>Log into your </Text>
+                    <Text
+                        style={{
+                            color: '#4285F4',
+                            fontSize: 28,
+                            fontFamily: 'Jost-Regular',
+                        }}
+                    >G</Text>
+                    <Text
+                        style={{
+                            color: '#DB4437',
+                            fontSize: 28,
+                            fontFamily: 'Jost-Regular',
+                        }}
+                    >o</Text>
+                    <Text
+                        style={{
+                            color: '#F4B400',
+                            fontSize: 28,
+                            fontFamily: 'Jost-Regular',
+                        }}
+                    >o</Text>
+                    <Text
+                        style={{
+                            color: '#4285F4',
+                            fontSize: 28,
+                            fontFamily: 'Jost-Regular',
+                        }}
+                    >g</Text>
+                    <Text
+                        style={{
+                            color: '#0F9D58',
+                            fontSize: 28,
+                            fontFamily: 'Jost-Regular',
+                        }}
+                    >l</Text>
+                    <Text
+                        style={{
+                            color: '#DB4437',
+                            fontSize: 28,
+                            fontFamily: 'Jost-Regular',
+                        }}
+                    >e{' '}</Text>
+                    <Text style={styles.log}>account</Text>
+                </View>
+                <View style={styles.container}>
+                    <GoogleSigninButton 
+                        onPress={googleLogin}
+                        style={{width: 200, height: 50}}
+                    />
+                </View>
+            </SafeAreaView>
+        );
 }
 
 const styles = StyleSheet.create({
@@ -136,3 +150,4 @@ const styles = StyleSheet.create({
         color: '#2A9DF4',
     },
 });
+
